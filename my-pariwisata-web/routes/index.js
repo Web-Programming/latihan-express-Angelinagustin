850ec321f21e852b557c7fdcf161ed6e4d6d5525
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2024-10-04 18:33:39 +0700

    ADD : paket wisata lanjutan
    .

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -7,11 +7,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/paketwisata',function (req, res, next){
-  res.render('paketwisata', {title:'Paket Wisata'});
+  let paketwisata= [
+    {'nama' :'wisata pulau kemarau', 'harga':5000},
+    {'nama' :'wisata punti kayu', 'harga':2000},
+    {'nama' :'wisata jakabaring', 'harga':7000},
+  ]
+  res.render('paketwisata', {
+    title:'Paket Wisata',
+    listpaketwisata: listpaketwisata});
 });
 
 router.get('/orderpaket',function (req, res, next){
-  res.render('orderpaket', {title:'Form Pemesanan Paket'});
+  let orderpaket= [
+    {'namapesanan' : 'budi',
+      'email' :'budi123@gmail.com', 
+      'noHP': '09293', 
+      'tglPesanan':'10 April 2024'},
+  ]
+  res.render('orderpaket', {
+    title:'Form Pemesanan Paket',
+    formulirpesanan :formulirpesanan
+  });
 });
 
 router.post('/orderdetail',function (req, res, next){
