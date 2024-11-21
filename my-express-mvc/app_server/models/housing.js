c94abedb45a896719b137740bc3e0f94c20efaa2
Angelinagustin <angelinagustin_2226240035@mhs.mdp.ac.id>
2024-11-21 20:38:41 +0700

    Add: models housing

--- my-express-mvc/app_server/models/housing.js
+++ my-express-mvc/app_server/models/housing.js
@@ -1,16 +1,41 @@
 const mongoose = require('mongoose');
 
-const housingLocationSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  photo: { type: String, required: true },
-  availableUnits: { type: Number, required: true },
-  wifi: { type: Boolean, required: true },
-  laundry: { type: Boolean, required: true },
+const housingSchema = new mongoose.Schema({
+    idhousing: {
+        type: Number, 
+        required: true
+    },
+    
+    name: { 
+        type: String, 
+        required: true 
+    },
+    city: {
+        type: String, 
+        required: true 
+    },
+    state: {
+        type: String, 
+        required: true 
+    },
+    photo: { 
+        type: String, 
+        required: true 
+    },
+    availableUnits: { 
+        type: Number, 
+        required: true 
+    },
+    wifi: { 
+        type: Boolean, 
+        required: true 
+    },
+    laundry: { 
+        type: Boolean, 
+        required: true 
+    },
 });
 
-const HousingLocation = mongoose.model('HousingLocation', housingLocationSchema);
+const Housing = mongoose.model('Housing', housingSchema);
 
-module.exports = HousingLocation;
+module.exports = Housing;
