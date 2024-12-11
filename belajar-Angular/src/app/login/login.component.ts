2deb7b028116f91127b2333029265b8c07b043fd
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2024-12-11 20:11:58 +0700

    ADD: AUTH VIDIO
    .

--- belajar-Angular/src/app/login/login.component.ts
+++ belajar-Angular/src/app/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from'@angular/router';
+import {Route, RouterModule} from'@angular/router';
+import { Router } from 'express';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,8 @@ formError: String = "";
 loginForm: FormGroup;
 
 //Inject class Router dan service authentication  
+router: Route =inject(Router);
+authService : AuthenticationService = inject(AuthenticationService);
 
 constructor(private fb: FormBuilder){
   this.loginForm = this.fb.group({
@@ -35,7 +39,17 @@ public onLoginSubmit(): void{
     const formData = this.loginForm.value;
     console.log('Form submitted', formData);
     //Panggil method loginAuth()
-    
+    const user =  {... this.loginForm.value} as User;
+    this.authService.loginAuth(user).then((res)=>{
+      if(res.message != null){
+        this.formError = res.message;
+      }else if (res.token != null){
+        this.authService.saveToken(res.token);
+        this.router.navigateByUrl('/');
+      }else{
+        this.formError = 'login failed. please try again';
+      }
+    });
   }else{
       this.formError = 'All fields are required, please try again';
   }
